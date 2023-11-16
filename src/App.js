9d1f5195538a22e4fc738271b5eaa24944e62f81
Cristian David Piedrahita Gomez <cristian.code.js@gmail.com>
2023-11-16 16:48:07 -0500

    Modificación al archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import Login from './Login';
-import Inicio from './Inicio';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Inicio from './components/Inicio';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = async (username, password) => {
-  try {
-    const response = await fetch('/auth.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
-    });
+    try {
+      const response = await fetch('/auth.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
+      });
 
-    if (response.ok) {
-      const result = await response.text();
-      if (result === 'success') {
-        // Autenticación exitosa
-        setIsLoggedIn(true);
+      if (response.ok) {
+        const result = await response.text();
+        if (result === 'success') {
+          setIsLoggedIn(true);
+        } else {
+          console.error('Credenciales incorrectas');
+        }
       } else {
-        // Autenticación fallida
-        console.error('Credenciales incorrectas');
+        console.error('Error en la solicitud al servidor');
       }
-    } else {
-      // Error en la solicitud al servidor
-      console.error('Error en la solicitud al servidor');
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
     }
-  } catch (error) {
-    console.error('Error en la solicitud:', error);
-  }
-};
-
+  };
 
   return (
     <Router>
-      <Switch>
-        <Route path="/login">
-          <Login onLogin={handleLogin} />
-        </Route>
-        <PrivateRoute path="/inicio" component={Inicio} isLoggedIn={isLoggedIn} />
-        <Redirect from="/" to="/login" />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/inicio" element={isLoggedIn ? <Inicio /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
     </Router>
   );
 };
-
-// Componente PrivateRoute para proteger rutas
-const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />
-    }
-  />
-);
 
 export default App;
