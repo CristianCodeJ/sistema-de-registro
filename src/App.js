0d0a5e4971c899b66bf6cceb908d651481ebcc54
Cristian David Piedrahita Gomez <cristian.code.js@gmail.com>
2023-11-16 12:36:02 -0500

    Implementación de los componentes Login y Inicio en App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Login from './Login';
+import Inicio from './Inicio';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async (username, password) => {
+  try {
+    const response = await fetch('/auth.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`,
+    });
+
+    if (response.ok) {
+      const result = await response.text();
+      if (result === 'success') {
+        // Autenticación exitosa
+        setIsLoggedIn(true);
+      } else {
+        // Autenticación fallida
+        console.error('Credenciales incorrectas');
+      }
+    } else {
+      // Error en la solicitud al servidor
+      console.error('Error en la solicitud al servidor');
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login onLogin={handleLogin} />
+        </Route>
+        <PrivateRoute path="/inicio" component={Inicio} isLoggedIn={isLoggedIn} />
+        <Redirect from="/" to="/login" />
+      </Switch>
+    </Router>
   );
-}
+};
+
+// Componente PrivateRoute para proteger rutas
+const PrivateRoute = ({ component: Component, isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
 
 export default App;
